aac98ad894630e07f7e11a312b0118e99fe4bb86
perez <perez_l@etna-alternance.net>
2026-01-15 16:08:33 +0100

    feat: adding component contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./components/Hero";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div>
-      <Hero />
+        <Hero />
+        <Contact />
     </div>
   );
 }
