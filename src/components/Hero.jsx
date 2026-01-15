7dcbc7461b859d87b3e6c104d7d97205b4f30c38
perez <perez_l@etna-alternance.net>
2026-01-15 15:22:09 +0100

    feat: beginning of the portfolio presentation

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <section id="hero">
+      <h1>Lélia Perez</h1>
+        <p>
+            Étudiante en informatique, je découvre et explore le monde de la programmation et du développement web. 
+            J’aime créer des projets concrets, apprendre de nouveaux langages et partager mes réalisations.
+        </p>
+            <button>Voir mes projets</button>
+    </section>
+  );
+}
+
+export default Hero;
