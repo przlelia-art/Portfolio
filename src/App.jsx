592a80f6a3b85aaa9390888b27a4816037d8738d
perez <perez_l@etna-alternance.net>
2026-01-15 14:37:29 +0100

    feat: adding the skeleton to assemble the components

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Projects />
+      <Skills />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
