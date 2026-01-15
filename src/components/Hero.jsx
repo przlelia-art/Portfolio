b259e007dea9e1e50688b229a035daaba1e2ce27
perez <perez_l@etna-alternance.net>
2026-01-15 15:53:19 +0100

    adding my photo for my presentation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,33 @@
 import React from "react";
+import MyPhoto from "../assets/photo_intra.jpg";
+
 
 function Hero() {
   return (
-    <section id="hero">
-      <h1>Lélia Perez</h1>
-        <p>
-            Étudiante en informatique, je découvre et explore le monde de la programmation et du développement web. 
-            J’aime créer des projets concrets, apprendre de nouveaux langages et partager mes réalisations.
-        </p>
+    <section 
+        id="hero" 
+        style={{
+            display: "flex", 
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "20px",
+            padding: "50px 20px"
+        }}
+        >
+        <img
+            src={MyPhoto}
+            alt="Moi"
+            style={{ width: "150px", borderRadius: "50%"}}
+        />
+        <div>
+            <h1>Lélia Perez</h1>
+            <p>
+                Étudiante en informatique, je découvre et explore le monde de la programmation et du développement web. 
+                J'aime créer des projets concrets, apprendre de nouveaux langages et partager mes réalisations.
+            </p>
             <button>Voir mes projets</button>
+
+        </div>
     </section>
   );
 }
