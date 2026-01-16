a3345ec84bf859b966d6e9c7d7a70268a16a2cb4
perez <perez_l@etna-alternance.net>
2026-01-16 11:30:55 +0100

    fix: correction of buttom contact

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,48 +1,47 @@
 import React from "react";
 import MyPhoto from "../assets/photo_intra.jpg";
 
-
 function Hero() {
   return (
-    <section 
-        id="hero" 
+    <section
+      id="hero"
+      style={{
+        textAlign: "center",
+        padding: "50px 20px",
+      }}
+    >
+      <img
+        src={MyPhoto}
+        alt="Moi"
         style={{
-            display: "flex", 
-            alignItems: "center",
-            justifyContent: "center",
-            gap: "20px",
-            padding: "50px 20px"
+          width: "150px",
+          borderRadius: "50%",
+          marginBottom: "20px", // espace entre photo et texte
         }}
+      />
+      <h1>Lélia Perez</h1>
+      <p>
+        Étudiante en informatique, je découvre et explore le monde de la programmation et du développement web. 
+        J'aime créer des projets concrets, apprendre de nouveaux langages et partager mes réalisations.
+      </p>
+
+      {/* Bouton en dessous du texte */}
+      <div style={{ marginTop: "20px" }}>
+        <a
+          href="#contact"
+          style={{
+            display: "inline-block",
+            padding: "10px 20px",
+            backgroundColor: "#4f46e5",
+            color: "#fff",
+            textDecoration: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
         >
-        <img
-            src={MyPhoto}
-            alt="Moi"
-            style={{ width: "150px", borderRadius: "50%"}}
-        />
-        <div>
-            <h1>Lélia Perez</h1>
-            <p>
-                Étudiante en informatique, je découvre et explore le monde de la programmation et du développement web. 
-                J'aime créer des projets concrets, apprendre de nouveaux langages et partager mes réalisations.
-            </p>
-        </div>
-
-         <a
-        href="#contact"
-        style={{
-          display: "inline-block",
-          marginTop: "20px",
-          padding: "10px 20px",
-          backgroundColor: "#4f46e5",
-          color: "#fff",
-          textDecoration: "none",
-          borderRadius: "5px",
-          cursor: "pointer",
-        }}
-      >
-        Me contacter
-      </a>
-
+          Me contacter
+        </a>
+      </div>
     </section>
   );
 }
