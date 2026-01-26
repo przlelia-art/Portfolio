15df0edb2dc83b283f98b367d6268ced4a576455
Lélia Perez <przlelia@gmail.com>
2026-01-26 16:00:12 +0100

    feat: adding the part of project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "./components/Hero";
+import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
 function App() {
   return (
     <>
       <Hero />
+      <Projects />
       <Contact />
     </>
   );
