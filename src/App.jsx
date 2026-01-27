d6a3aca1b3600500b8710214c9cfd8e82129442b
Lélia Perez <przlelia@gmail.com>
2026-01-27 14:41:37 +0100

    feat: adding skills

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Hero />
       <Projects />
+      <Skills />
       <Contact />
       <Footer />
     </>
