fdd1c967c2c18abd01d1407c6eefecf6c52309be
Lélia Perez <przlelia@gmail.com>
2026-01-27 11:14:55 +0100

    adding header of app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
@@ -6,6 +7,7 @@ import Contact from "./components/Contact";
 function App() {
   return (
     <>
+      <Header />
       <Hero />
       <Projects />
       <Contact />
