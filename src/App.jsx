c243a825105b59aca75a4c03334d800e09e5239d
Lélia Perez <przlelia@gmail.com>
2026-01-27 14:27:40 +0100

    adding the footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Projects />
       <Contact />
+      <Footer />
     </>
   );
 }
