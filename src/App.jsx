fb3e268d82c4edfaa4ba3e022212b6a927270291
lelia <perez_l@etna-alternance.net>
2026-06-22 10:06:55 +0200

    feat(app.jsx): add import of personnal project file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
+import PersonalProjects from "./components/PersonalProjects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
